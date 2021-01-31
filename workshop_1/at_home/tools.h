f65d8e6d5071809507651fcde8643343a048bbcb
TheOG-2670 <56968507+TheOG-2670@users.noreply.github.com>
2021-01-31 11:58:46 -0500

    Add files via upload

---
+++ workshop_1/at_home/tools.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef NAMESPACE_TOOLS_H
+#define NAMESPACE_TOOLS_H
+
+namespace sict
+{
+
+	// Displays the user interface menu
+	int menu();
+
+	// Performs a fool-proof integer entry
+	int getInt(int min, int max);
+}
+
+#endif
